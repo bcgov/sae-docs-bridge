f65208b5da05c693264e3e79a1759b2f10f6189e
Joshua Jones <joshua@general-metrics.com>
2020-05-21 11:59:02 -0700

    Add license and allow whitelist config to be empty.

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const format = config.get('morganFormat');
 
 const corsOptions = {
   origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (whitelist.length === 0 || whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS ' + origin));
